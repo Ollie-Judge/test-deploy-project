1f1fc2179dd8fa35c0f41344d68bf42efae04e0b
Ollie-Judge <ollie.judge@hotmail.com>
2024-01-19 13:40:54 +0000

    added new pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import MainText from "./components/MainText";
 function App() {
   return (
     <>
+      <a href="/new" target="_blank" rel="noopener noreferrer">
+        new page
+      </a>
       <MainText />
     </>
   );
---
+++ src/pages/newPage.jsx
@@ -0,0 +1,14 @@
+function newPage() {
+  return (
+    <>
+      <a href="/" target="_blank" rel="noopener noreferrer">
+        home
+      </a>
+      <div>
+        <h2>This is the new page</h2>
+      </div>
+    </>
+  );
+}
+
+export default newPage;
